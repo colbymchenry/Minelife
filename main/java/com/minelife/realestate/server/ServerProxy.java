971c43165354f949b0d86f772c4393b709192846
SethPeden <seth.peden@gmail.com>
2017-08-12 13:47:05 -0400

    Started using packets in Real Estate. Also setup a config file for Real Estate. SimpleConfig needs to be edited to use NBTTagLists, as you can write any value to the SimpleConfig, but when reading from it from memory, all values must be considered Strings. Also added in KeyBinding to KEY_C to cancel selections.

--- main/java/com/minelife/realestate/server/ServerProxy.java
+++ main/java/com/minelife/realestate/server/ServerProxy.java
@@ -10,9 +10,11 @@ public class ServerProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         try {
-            ModRealEstate.config = new MLConfig("realestate");
-            ModRealEstate.config.addDefault("price_per_block", 2);
-            ModRealEstate.config.save();
+            ModRealEstate.config = new MLConfig("real_estate");
+            if (!ModRealEstate.config.getKeys(true).contains("price_per_block")) {
+                ModRealEstate.config.addDefault("price_per_block", 2);
+                ModRealEstate.config.save();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
