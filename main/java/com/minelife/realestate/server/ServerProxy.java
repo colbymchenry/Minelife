c499dde5a7c61391df8d756ed6ef8e17ec840073
SethPeden <seth.peden@gmail.com>
2017-08-15 20:30:25 -0400

    Added EstateRegion so as to not collide with Regions.

--- main/java/com/minelife/realestate/server/ServerProxy.java
+++ main/java/com/minelife/realestate/server/ServerProxy.java
@@ -1,9 +1,13 @@
 package com.minelife.realestate.server;
 
 import com.minelife.CommonProxy;
+import com.minelife.Minelife;
+import com.minelife.realestate.EstateRegion;
 import com.minelife.realestate.ModRealEstate;
 import com.minelife.util.MLConfig;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import java.util.UUID;
 
 public class ServerProxy extends CommonProxy {
 
@@ -12,6 +16,9 @@ public class ServerProxy extends CommonProxy {
         ModRealEstate.config = new MLConfig("real_estate");
         ModRealEstate.config.addDefault("price_per_block", 2);
         ModRealEstate.config.save();
+        Minelife.SQLITE.query("CREATE TABLE IF NOT EXISTS ESTATE_REGIONS (uuid VARCHAR(36) NOT NULL DEFAULT '" + UUID.randomUUID().toString() + "', " +
+                "world VARCHAR(60) NOT NULL, minX INT NOT NULL, minY INT NOT NULL, minZ INT NOT NULL, maxX INT NOT NULL, maxY INT NOT NULL, maxZ INT NOT NULL)");
+        EstateRegion.initRegions();
     }
 
 }
