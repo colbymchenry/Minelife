a3e8982f42c4ad4b373c1ebdc031e1910367c2c6
colbymchenry <colbymchenry@gmail.com>
2017-09-16 18:21:34 -0400

    Did a lot of work with the prison setup.

--- main/java/com/minelife/police/client/ClientProxy.java
+++ main/java/com/minelife/police/client/ClientProxy.java
@@ -2,6 +2,7 @@ package com.minelife.police.client;
 
 import com.minelife.CommonProxy;
 
+// TODO: Render on screen if in prison the time left to serve and the bail that they owe
 public class ClientProxy extends CommonProxy {
 
 }
