065f5e83f9deac8b1413ed9063f49fc62da376e0
colbymchenry <colbymchenry@gmail.com>
2017-12-02 12:54:53 -0500

    Started working more on the Gui of the shop block and activating the shop block.

--- main/java/com/minelife/shop/BlockShopBlock.java
+++ main/java/com/minelife/shop/BlockShopBlock.java
@@ -2,7 +2,11 @@ package com.minelife.shop;
 
 import buildcraft.core.lib.block.BlockBuildCraft;
 import com.minelife.Minelife;
+import com.minelife.shop.client.GuiShopBlock;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
@@ -13,6 +17,8 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class BlockShopBlock extends BlockBuildCraft {
 
@@ -35,19 +41,33 @@ public class BlockShopBlock extends BlockBuildCraft {
 
         TileEntityShopBlock tileEntityATM = (TileEntityShopBlock) world.getTileEntity(x, y, z);
         tileEntityATM.setFacing(l == 0 ? EnumFacing.NORTH : l == 1 ? EnumFacing.EAST : l == 2 ? EnumFacing.SOUTH : EnumFacing.WEST);
+        tileEntityATM.setOwner(player.getUniqueID());
     }
 
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-        if (world.isRemote) return true;
 
-        try {
-//            Minelife.NETWORK.sendTo(new PacketOpenATM(ModEconomy.getPin(player.getUniqueID()).isEmpty()), (EntityPlayerMP) player);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (world.getTileEntity(x, y, z) != null && world.getTileEntity(x, y, z) instanceof TileEntityShopBlock) {
+            TileEntityShopBlock tileEntityShopBlock = (TileEntityShopBlock) world.getTileEntity(x, y, z);
+
+            if (world.isRemote) {
+                clientBlockActivated(tileEntityShopBlock, player);
+            } else {
+                if(tileEntityShopBlock.getOwner() != null && !tileEntityShopBlock.getOwner().equals(player.getUniqueID())) {
+                    // TODO: Do purchase
+                }
+            }
         }
 
+
         return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void clientBlockActivated(TileEntityShopBlock tileEntityShopBlock, EntityPlayer player) {
+        if (Objects.equals(tileEntityShopBlock.getOwner(), player.getUniqueID())) {
+            Minecraft.getMinecraft().displayGuiScreen(new GuiShopBlock(tileEntityShopBlock));
+        }
     }
 
     @Override
--- main/java/com/minelife/shop/client/GuiShopBlock.java
+++ main/java/com/minelife/shop/client/GuiShopBlock.java
@@ -1,4 +1,88 @@
 package com.minelife.shop.client;
 
-public class GuiShopBlock {
+import com.minelife.shop.TileEntityShopBlock;
+import com.minelife.util.client.GuiFakeInventory;
+import com.minelife.util.client.GuiUtil;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
+
+import java.awt.Color;
+
+public class GuiShopBlock extends GuiScreen {
+
+    private GuiFakeInventory fake_inventory;
+
+    private static final Color slot_color = new Color(0x919191);
+    private TileEntityShopBlock tile_entity;
+    private int bg_width = 100, bg_height = 200, xPosition, yPosition;
+    private GuiTextField price_field;
+    private int slot_to_sale;
+
+    public GuiShopBlock(TileEntityShopBlock tile_entity) {
+        this.tile_entity = tile_entity;
+    }
+
+    @Override
+    public void drawScreen(int mouse_x, int mouse_y, float f) {
+        super.drawScreen(mouse_x, mouse_y, f);
+        GuiUtil.drawDefaultBackground(xPosition, yPosition, bg_width, bg_height);
+        price_field.drawTextBox();
+       fake_inventory.draw(mouse_x, mouse_y);
+    }
+
+    @Override
+    protected void keyTyped(char key_char, int key_id) {
+        super.keyTyped(key_char, key_id);
+        price_field.textboxKeyTyped(key_char, key_id);
+    }
+
+    @Override
+    protected void mouseClicked(int mouse_x, int mouse_y, int mouse_btn) {
+        super.mouseClicked(mouse_x, mouse_y, mouse_btn);
+        price_field.mouseClicked(mouse_x, mouse_y, mouse_btn);
+        if(fake_inventory.getClickedSlot(mouse_x, mouse_y)  != -1) this.slot_to_sale = fake_inventory.getClickedSlot(mouse_x, mouse_y);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton btn) {
+        super.actionPerformed(btn);
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+        xPosition = (this.width - bg_width) / 2;
+        yPosition = (this.height - bg_height) / 2;
+        price_field = new GuiTextField(fontRendererObj, xPosition + ((bg_width - 75) / 2), yPosition + 10, 75, 20);
+
+        int xOffset = xPosition;
+        int yOffset = yPosition;
+
+        fake_inventory = new GuiFakeInventory(mc) {
+            @Override
+            public void setupSlots()
+            {
+                // players hotbar
+                for (int x = 0; x < 9; ++x) {
+                    this.slots.add(new ItemSlot(mc.thePlayer.inventory.mainInventory[x], new java.awt.Rectangle(xOffset + x * 18, yOffset + 58, 16, 16), x));
+                }
+
+                // players inventory
+                for (int y = 0; y < 3; ++y) {
+                    for (int x = 0; x < 9; ++x) {
+                        this.slots.add(new ItemSlot(mc.thePlayer.inventory.mainInventory[x + y * 9 + 9], new java.awt.Rectangle(xOffset + x * 18, yOffset + y * 18, 16, 16), x + y * 9 + 9));
+                    }
+                }
+            }
+        };
+
+        fake_inventory.slotColor = new Color(0x919191);
+    }
+
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
+        price_field.updateCursorCounter();
+    }
 }
